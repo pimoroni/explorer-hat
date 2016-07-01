c88ec7f370dd9a3b46af3b2ceca424256d50ee61
RogueM <chaosgen6@googlemail.com>
2016-07-01 18:27:08 +0000

    added df-python to Build-depends

--- library/makedeb.sh
+++ library/makedeb.sh
@@ -3,10 +3,8 @@
 gettools="yes"
 
 if [ $gettools == "yes" ]; then
-    sudo apt-get update && sudo apt-get upgrade
-    sudo apt-get install devscripts debhelper
-    sudo apt-get install python-all python-setuptools
-    sudo apt-get install python3-all python3-setuptools
+    sudo apt-get update && sudo apt-get install devscripts debhelper
+    sudo apt-get install python-all python-setuptools python3-all python3-setuptools
 fi
 
 debuild -Ipackage
