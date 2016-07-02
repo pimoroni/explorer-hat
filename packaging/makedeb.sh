ced7052dddfa4872d57341879f313ac4ec9c68b5
RogueM <chaosgen6@googlemail.com>
2016-07-03 00:19:16 +0100

    cleanup in make deb script

--- packaging/makedeb.sh
+++ packaging/makedeb.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 gettools="yes"
+cleanup="yes"
 
 if [ $gettools == "yes" ]; then
     sudo apt-get update && sudo apt-get install build-essential dh-make dh-python debhelper devscripts
     sudo apt-get install python-all python-setuptools python3-all python3-setuptools
 fi
 
-cd ..
+cd ../library
 debuild -Ipackage
+
+mv ../*.build ../packaging
+mv ../*.changes ../packaging
+mv ../*.tar.xz ../packaging
+mv ../*.deb ../packaging
+mv ../*.dsc ../packaging
+
+if [ $cleanup == "yes" ]; then
+    dh clean
+    rm -R ./build
+fi
 
 exit 0
