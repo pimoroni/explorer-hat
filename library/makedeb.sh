a23e7f74de4429d0c11137f830ac7b6bc7f03405
RogueM <chaosgen6@googlemail.com>
2016-07-02 01:19:01 +0100

    adding tools for build

--- library/makedeb.sh
+++ library/makedeb.sh
@@ -3,7 +3,7 @@
 gettools="yes"
 
 if [ $gettools == "yes" ]; then
-    sudo apt-get update && sudo apt-get install devscripts debhelper
+    sudo apt-get update && sudo apt-get install build-essential df-make debhelper devscripts
     sudo apt-get install python-all python-setuptools python3-all python3-setuptools
 fi
 
